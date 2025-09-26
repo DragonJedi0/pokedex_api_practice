f6eb305134d9335477f42df250ec2c26ca7a3c38
Samuel Ybarra <sybarra2076@gmail.com>
2025-09-26 10:24:43 -0500

    adding cache class

---
+++ src/pokecache.ts
@@ -0,0 +1,49 @@
+export type CacheEntry<T> = {
+    createdAt: number,
+    value: T,
+}
+
+export class Cache {
+    #cache = new Map<string, CacheEntry<any>>();
+    #reapIntervalId: NodeJS.Timeout | undefined = undefined;
+    #interval: number;
+
+    constructor(number: number){
+        this.#interval = number;
+        this.#startReapLoop();
+    }
+
+    add<T>(key: string, val: T){
+        const cacheData: CacheEntry<T> = {
+            createdAt: Date.now(),
+            value: val,
+        }
+        this.#cache.set(key, cacheData);
+    }
+
+    get<T>(key: string){
+        if(this.#cache.has(key)){
+            return this.#cache.get(key);
+        }
+        return undefined;
+    }
+
+    #reap(){
+        this.#cache.forEach(
+            (value, key) => {
+                if(value.createdAt > Date.now() - this.#interval){
+                    this.#cache.delete(key);
+                }
+            }
+        );
+    }
+
+    #startReapLoop(){
+        this.#reapIntervalId = setInterval(this.#reap, this.#interval);
+    }
+
+    stopReapLoop(){
+        clearInterval(this.#reapIntervalId);
+        this.#reapIntervalId = undefined;
+    }
+}
